f695daa3819e1292d322584aa0df3e479f53eb67
laviealon <79278018+laviealon@users.noreply.github.com>
2021-07-14 23:21:24 -0400

    Completed method <NeuralNetwork.get_grad>. To improve efficiency, a good TODO would be to change the implementation of <NeuralNetwork._get_a_derivs_array>, however changes to dependent methods would have to be made. Main current TODO is to create method <NeuralNetwork.adjust>, which adjusts the neural network based on the gradient.

--- network.py
+++ network.py
@@ -1,17 +1,30 @@
 from __future__ import annotations
 import numpy as np
-from typing import List, Tuple, Optional
-from data_manager import DataPair, collect_sample
-from random import uniform
-from math import exp
+from typing import List, Tuple
+from data_manager import DataPair
 
 
 class NeuralNetwork:
     """A Neural Network with an input layer, 2 hidden layers,
     and an output layer.
+
+    === Attributes ===
+    fl_size: the size of the network's first layer (input layer).
+    hl_size: the size of the network's 2 hidden layers.
+    ll_size: the size of the network's last layer (output layer).
+    weights: a list of 3 matrices containing the synaptic weights between
+        the network's neurons.
+    biases: a list of 3 matrices containing the biases the network's neurons
+        (excludes the input layer neurons as they don't require biases).
     """
+    fl_size: int
+    hl_size: int
+    ll_size: int
+    weights: List[np.array]
+    biases: List[np.array]
 
     def __init__(self, fl_size: int, ll_size: int, hl_size: int) -> None:
+        """Initialize a neural network."""
         self.fl_size = fl_size
         self.hl_size = hl_size
         self.ll_size = ll_size
@@ -33,7 +46,20 @@ class NeuralNetwork:
         # matrix of biases of last layer
         self.biases.append(10 * np.random.random((ll_size, 1)) - 5)
 
-    def compute(self, first_layer_activations: np.array) -> np.array:
+    def compute(self, first_layer_activations: np.array) -> List[np.array]:
+        """Compute the output of this neural network with
+        <first_layer_activations> as input.
+
+        Return a list of the following form (indices are listed in brackets):
+        [output layer, (0)
+        z1,            (1)
+        z2,            (2)
+        z3]            (3)
+
+        Preconditions:
+        - <first_layer_activations> must be an array of size
+            <self.fl_size> by 1
+        """
         # calculate activations of hidden layer 1 using sigmoid
         # of matrix multiplication
         z1 = np.dot(self.weights[0], first_layer_activations) + self.biases[0]
@@ -48,10 +74,74 @@ class NeuralNetwork:
         l3 = sigmoid_vec(z3)
         return l3
 
+    def get_grad(self, data_pair: DataPair) -> \
+            Tuple[List[np.array], List[np.array]]:
+        """Return the gradient of the cost function of this neural network.
+
+        The gradient is returned as tuple, where the first element is a list
+        of arrays containing the partial derivative of the cost function wrt
+        the network's weights, and the first element is a list of arrays
+        containing the partial derivative of the cost function wrt the
+        network's biases.
+
+        Preconditions:
+        - <exo_outp> is an array of size <self.ll_size> by 1.
+        """
+        inp = data_pair.inp
+        exp_outp = data_pair.exp_output
+        z_vals = self.compute(inp)[1:]
+        z1 = z_vals[0]
+        l1 = sigmoid_vec(z1)
+        z2 = z_vals[1]
+        l2 = sigmoid_vec(z2)
+        z3 = z_vals[2]
+        l3 = sigmoid_vec(z3)
+        z_layers = [z1, z2, z3]
+        layers = [inp, l1, l2, l3]
+        a_derivs = self._get_a_derivs_array(inp, z1, z2, z3, exp_outp)
+        # Compute the partial derivatives wrt the network's weights
+        weights_derivs = []
+        for L in range(1, 4):
+            weight_derivs_L = []
+            weights_array = self.weights[L - 1]
+            for i in range(len(weights_array)):
+                weight_derivs_L_i = []
+                row = weights_array[i]
+                for j in range(len(row)):
+                    dzdw = layers[L][j]
+                    dadz = sigmoid_deriv(z_layers[L - 1][i])
+                    dCda = a_derivs[L][i]  # if modify method <_get_a_derivs_array, change to L - 1>
+                    weight_derivs_L_i.append(dzdw * dadz * dCda)
+                weight_derivs_L.append(weight_derivs_L_i[:])
+            weights_derivs.append(np.array(weight_derivs_L[:]))
+        # Compute the partial derivatives wrt the network's biases
+        biases_derivs = []
+        for L in range(1, 4):
+            biases_derivs_L = []
+            biases_array = self.biases[L - 1]
+            for i in range(len(biases_array)):
+                dzdb = -1
+                dadz = sigmoid_deriv(z_layers[L - 1][i])
+                dCda = a_derivs[L][i]
+                biases_derivs_L.append(dzdb * dadz * dCda)
+            biases_derivs.append(biases_derivs_L[:])
+        return weights_derivs, biases_derivs
+
     def _get_a_derivs_array(self, l0: np.array, z1: np.array, z2: np.array,
                             z3: np.array, exp_outp: np.array) -> List[np.array]:
         """Compute the partial derivatives of the cost function wrt the
-        activations of the neurons in this neural network."""
+        activations of the neurons in this neural network.
+
+        Return a list of 4 arrays, each array represeting a layer in the
+        neural network. Each array contains the partial derivatives of the cost
+        function wrt the activations of the neurons corresponding to the layer
+        the array represents.
+
+        Preconditions:
+        - <l0>, <z1>, <z2>, and <z3> must be the appropriate size relative
+            to the layer they correspond to.
+        - <exp_outp> must be a numpy array of size <self.ll_size> by 1.
+        """
         arrays = []
         # compute z-layers (see README) as activations
         l1 = sigmoid_vec(z1)
